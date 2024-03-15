5d482133296811d6953659c6ce247aa514e7e4d7
Rishil-NWM <156384714+Rishil-NWM@users.noreply.github.com>
2024-03-15 17:00:59 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rishil Reddy' });
+  res.render('mydata', { title: 'Rishil Reddy' });
 });
 
 module.exports = router;
